7c99335f2f8b927c3ab2053e29945a106c68b5e2
Cristobal <Cristobal@cristobalreyes.com>
2026-03-11 23:06:47 -0600

    Fix CORS

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
+
 app.use(express.json());
 app.use(express.static('public'));
 
